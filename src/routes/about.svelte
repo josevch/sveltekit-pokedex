f87cd9865e3313faaf919943183e62b298abea9a
Jose Von Chong <javch1995@gmail.com>
2022-06-01 02:21:24 -0500

    trying the sveltekit pokedex

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>About the Pokedex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p class="text-center">Bueno este es el about, esto es un demo de un pokedex</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { pokemon } from '../stores/pokestore';
+	import PokemanCard from '../components/pokemanCard.svelte';
+</script>
+
+<svelte:head>
+	<title>Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Sveltekit Pokedex</h1>
+
+{#each $pokemon as pokeman}
+	<PokemanCard {pokeman} />
+{/each}
