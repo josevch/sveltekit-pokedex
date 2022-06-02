64c0d29833a8214061c171d0668925c92dae35ce
Jose Von Chong <javch1995@gmail.com>
2022-06-02 16:35:05 -0500

    updates

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -4,7 +4,7 @@
 
 <a
 	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center  "
-	href={'/pokeman/${pokeman.id}'}
+	href={'/pokemon/${pokeman.id}'}
 >
 	<img class="w-40 h-40" src={pokeman.image} alt={pokeman.name} />
 	<h2 class="uppercase font-semibold ">{pokeman.id}. {pokeman.name}</h2>
